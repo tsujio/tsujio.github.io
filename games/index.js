a0b829301b9e2dd5fc2bcb1f156c1fd5488cceac
tsujio <ntsujio@gmail.com>
2021-08-28 00:52:39 +0900

    Update

--- games/index.js
+++ games/index.js
@@ -1,43 +1,20 @@
 (() => {
-  const games = [
-    {
-      title: "Birdman Challenge",
-      link: "https://www.tsujio.org/games/game.html?title=birdman",
-      image: "https://github.com/tsujio/game-birdman/blob/main/image.gif?raw=true",
-      description: "鳥人間コンテストを題材にしたゲームです。",
-    },
-    {
-      title: "Maxwell's Demon",
-      link: "https://www.tsujio.org/games/game.html?title=maxwells-demon",
-      image: "https://github.com/tsujio/game-maxwells-demon/blob/main/image.gif?raw=true",
-      description: "マクスウェルの悪魔を題材にしたゲームです。",
-    },
-    {
-      title: "Spotted Garden Eel",
-      link: "https://www.tsujio.org/games/game.html?title=spotted-garden-eel",
-      image: "https://github.com/tsujio/game-spotted-garden-eel/blob/main/image.gif?raw=true",
-      description: "チンアナゴを題材にしたゲームです。",
-    },
-    {
-      title: "Cosmic Velocity",
-      link: "https://www.tsujio.org/games/game.html?title=cosmic-velocity",
-      image: "https://github.com/tsujio/game-cosmic-velocity/blob/main/image.gif?raw=true",
-      description: "宇宙速度を題材にしたゲームです。",
-    },
-  ]
-
   window.onload = () => {
-    const tmpl = document.querySelector("#game-container-template").content
-    const gamesContainer = document.querySelector("#games-container")
+    fetch("https://www.tsujio.org/games/games.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const tmpl = document.querySelector("#game-container-template").content
+        const gamesContainer = document.querySelector("#games-container")
 
-    for (const game of games) {
-      const container = document.importNode(tmpl, true)
-      container.querySelector(".game-title").textContent = game.title
-      container.querySelector(".game-link").href = game.link
-      container.querySelector(".game-image").src = game.image
-      container.querySelector(".game-description").textContent = game.description
+        for (const game of data.games) {
+          const container = document.importNode(tmpl, true)
+          container.querySelector(".game-title").textContent = game.title
+          container.querySelector(".game-link").href = game.link
+          container.querySelector(".game-image").src = game.image
+          container.querySelector(".game-description").textContent = game.description
 
-      gamesContainer.appendChild(container)
-    }
+          gamesContainer.appendChild(container)
+        }
+      })
   }
 })()
